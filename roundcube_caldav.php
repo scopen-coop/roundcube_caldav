876e9c753fd01f3924c7900b5bcb2c7879a638b3
Noé <noe@scopen.fr>
2024-11-25 10:37:48 +0100

    test

--- roundcube_caldav.php
+++ roundcube_caldav.php
@@ -706,7 +706,7 @@ class roundcube_caldav extends rcube_plugin
                 $response['used_event'] = $event;
             }
             
-             $this->rcmail->output->command('display_message', $this->gettext($event), 'error');
+             $this->rcmail->output->command('display_message', $event, 'error');
             set_participants_characteristics_and_set_buttons_properties(
                     $response['used_event'],
                     $response
